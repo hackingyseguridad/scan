4a8c1d79ca9c0541ed94705aa81f1a20f4ada47e
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-03-29 00:32:07 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -2,9 +2,9 @@
 read -p opcion
 case $opcion in
 0) echo " " ;;
-1) nmap -iL ip.txt -Pn -f --open -sU -sS -sC -sV -O --max-retries 1 -p 53 -oG resultado.txt;; 
-2) echo "2";;
-3) echo "3";;
+1) nmap -iL ip.txt -Pn -f --open -sU -sS -sC -sV -O --max-retries 1 -F -oG resultado.txt;; 
+2) nmap -iL ip.txt -Pn -f --open -sU -sS -sC -sV -O --max-retries 1 -oG resultado.txt;; 
+3) nmap -iL ip.txt -Pn -f --open -sU -sS -sC -sV -O --max-retries 1 -p 0-65535 -oG resultado.txt;; 
 *) echo "uso: ./scan.sh"
 echo "Fichero: ip.txt listado IP/rangos a escanear"
 echo "Fichero: resultado.txt se genera al finalizar"
