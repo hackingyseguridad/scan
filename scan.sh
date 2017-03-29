d44018ef131267a5171191fd83384e3f6b7c28e7
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-03-29 09:45:44 +0200

    Add files via upload

---
+++ scan.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Ejecutar en consola: sh scan.sh"
+echo "Incluir en la misma carpeta fichero ip.txt con las IP o rangos a escanear"
+echo "Se generara automaticamente en la misma carpeta el fichero resultado.txt"
+echo "Pulsa opcion escaneo UDP/TCP: 0 - 9:"
+echo "1) Rapido 100p, 2) Normal 1000 p, 3) Completo 65536 p, 0) Salir"
+read x
+case $x in
+1)
+echo ""; nmap -iL ip.txt -Pn -f --open -sU -sT -sC -sV -O --max-retries 1 -F -oG -A resultado.txt
+;;
+2)
+echo ""; nmap -iL ip.txt -Pn -f --open -sU -sT -sC -sV -O --max-retries 1  -oG resultado.txt
+;;
+3)
+echo ""; nmap -iL ip.txt -Pn -f --open -sU -sT -sC -sV -O --max-retries 1 -p 0-65535 -oG resultado.txt
+;;
+*)
+echo "Opcion no valida!"
+;;
+esac
