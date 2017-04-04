f2c52d0b26b449718c101b962e652bd3e8ad5129
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-04-04 14:03:19 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -4,6 +4,7 @@
 #(c) Antonio Taboada 4/4/2017
 #En modo consola dar permisos de ejecucion: chmod 777 scan.sh
 #############################################################
+echo +++ Simple script para escaneos con https://nmap.org +++
 echo "Uso: sh scan.sh"			
 echo "Incluir en misma carpeta ip.txt con rangos a escanear"		
 echo "Generara automaticamente misma carpeta resultado.txt"
