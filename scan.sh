b62a90497222123324309b0683fe6034e3a339b3
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-04-07 22:10:19 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -57,7 +57,7 @@ p)
 echo ""; nmap -iL ip.txt -sn -oG resultado.txt; tail resultado.txt
 ;;
 t)
-echo ""; nmap -iL ip.txt --traceroute -oG resultado.txt; tail resultado.txt 
+echo ""; nmap -iL ip.txt --traceroute > resultado.txt
 ;;
 *)
 echo "Opcion no valida!"
