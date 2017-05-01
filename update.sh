7f1636790f56a57bb4ad7c16c4b10e8d6a5b6482
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-05-01 16:42:36 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Actualiza solo el script bash. Version Beta (c) Antonio Taboada 2017
 wget https://raw.githubusercontent.com/hackingyseguridad/scan/master/scan.sh
+apt-get upgrade nmap
