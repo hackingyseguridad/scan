1bafce57f368c9a605391f0b7a7797e508a94604
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2017-08-26 00:37:21 +0200

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -7,10 +7,12 @@ chmod 777 bgp2ip
 chmod 777 ip2bgp
 chmod 777 miip
 chmod 777 tracebgp
+chmod 777 scantcp
 cp bgp2ip /sbin/
 cp ip2bgp /sbin/
 cp miip /sbin
 cp tracebgp /sbin/
+cp scantcp /sbin/
 apt-get install nmap
 git clone https://github.com/glennzw/shodan-hq-nse
 cp shodan-hq-nse/shodan-hq.nse /usr/share/nmap/scripts/
