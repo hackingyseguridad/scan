43b283f5c0feb8c30f990ca4f0e346c8030d1586
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-02-06 22:05:36 +0100

    Update cve.sh

--- cve.sh
+++ cve.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# https://github.com/scipag/vulscan
+# https://github.com/vulnersCom/nmap-vulners
 nmap -iL ip.txt -Pn --open -sVUT --max-retries 1 -O  --defeat-rst-ratelimit --script=vulners.nse 
