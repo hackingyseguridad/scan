79c865a74b71d6dc0cbe1dd0b523bf9ab2a27bfd
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-06-11 16:20:24 +0200

    Update hostalive.sh

--- hostalive.sh
+++ hostalive.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-for n in `ip.txt`; do 
-if ping -c 1 -W 1 "$n"; then
-  echo "$n Up"
+for n in `cat ip.txt`; do if ping $n -c 1 -W 1 >/dev/null; then
+        echo $n "Up"
 else
-  echo "$n Down"
+        echo $n "Down"
 fi
-done
+done;
