f811a3cde68c775f5ffa7223f274090780c792af
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-06-11 15:52:50 +0200

    Update hostalive.sh

--- hostalive.sh
+++ hostalive.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for n in `ip.txt`; do 
 if ping -c 1 -W 1 "$n"; then
-  echo "$hostname_or_ip_address is alive"
+  echo "$n Up"
 else
-  echo "$hostname_or_ip_address is pining for the fjords"
+  echo "$n Down"
 fi
-nc -zv $n 80 443 -w 1; done
+done
