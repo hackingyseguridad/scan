29a86ecb6cd0aac71ff1adfb53cf32a7bc817eaf
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-07-22 16:14:30 +0200

    Update scanipv6local.sh

--- scanipv6local.sh
+++ scanipv6local.sh
@@ -6,4 +6,5 @@ echo "Uso.: sh scanipv6local.sh"
 
 #!/bin/sh
 ping6 -I eth0 -c 2 ff02::1 | grep DUP | awk '{print substr($4, 1, length($4)-1)}' > ip.txt
+tail ip.txt
 nmap -6 -Pn sVC -iL ip.txt --open
