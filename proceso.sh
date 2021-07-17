3768c3e64745a19c4f412ef5cef3fda47dacf253
hackingyseguridad <antonio_taboada@hotmail.es>
2021-07-17 17:06:58 +0200

    Update proceso.sh

--- proceso.sh
+++ proceso.sh
@@ -11,7 +11,7 @@ sudo apt-get install xsltproc
 
 while :
 
-do sudo nmap $1 192.168.1.0/24 -Pn -sVC -O -T5- -p 21,22,23,25,53,80,139,222,389,443,445,554,631,966,1023,1723,1080,2022,2222,3389,5900,7443,8080,8443,8888,10000,22222 --open --script=ssl* --script=vuln* -oX index.xml
+do sudo nmap $1 192.168.1.0/24 -Pn -sVC -O -T5- --open -p 21,22,23,25,53,80,139,222,389,443,445,554,631,966,1023,1723,1080,2022,2222,3389,5900,7443,8080,8443,8888,10000,22222 --open --script=ssl* --script=vuln* -oX index.xml
 
 sudo xsltproc index.xml -o /var/www/html/index.html
 echo
