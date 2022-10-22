691909e9699cbd562a7e07501a5e91c652cdb209
hackingyseguridad <antonio_taboada@hotmail.es>
2022-10-22 21:51:46 +0200

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -5,5 +5,5 @@
 # Los resultados con estado unfiltered http son puertos filtrados. con nmap envia ACK  y se obtiene un RST
 # 
 #
-nmap -iL http2.txt -Pn -g0 --open -sACV  -T4 -f -O --max-retries 1 --defeat-rst-ratelimit --script firewall-bypass  -p 80,10443,5411,443,4433,8443,4443,444,9443,7443,1723,6443,8009,10000,55443,9000,8081,8880,8889,9001 -oG resultado_firewall2.txt
+nmap -iL http2.txt -Pn -g0 --open -sACV  -T4 -f -O --max-retries 1 --defeat-rst-ratelimit --script firewall-bypass  -p 80,10443,5411,443,4433,8443,4443,444,9443,7443,1723,6443,8009,10000,55443,9000,8081,8880,8889,9001 -oG resultado_firewall.txt
 cat resultado_firewall.txt | grep unfiltered
