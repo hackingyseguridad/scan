cb732eefa9039fbed7d08e05fd27ea1615389825
hackingyseguridad <antonio_taboada@hotmail.es>
2023-02-15 17:00:37 +0100

    Update cve.sh

--- cve.sh
+++ cve.sh
@@ -2,4 +2,4 @@
 # https://github.com/scipag/vulscan
 # https://github.com/vulnersCom/nmap-vulners
 nmap -iL ip.txt -Pn --open -sVUT -F -O --max-retries 1 -O  --defeat-rst-ratelimit --script=vulners.nse 
-nmap -iL ip.txt -Pn --open -sVUT -F -O --max-retries 1 -O  --defeat-rst-ratelimit --script=vulscan/vulscan.nse
+nmap -iL ip.txt -Pn --open -sVUT -F -O --max-retries 1 -O  --defeat-rst-ratelimit --script=vulscan/vulscan.nse --script-args vulscandb=cve.csv
