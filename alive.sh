38d8d326ba4a6b4749b98c457a6f470b8363db2c
hackingyseguridad <antonio_taboada@hotmail.es>
2023-03-01 16:08:54 +0100

    Update alive.sh

--- alive.sh
+++ alive.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Escaneo rapido para ver activos IP con respuesta ping
 # hacking y seguridad 2023
-# 
+# masscan -iL ip.txt --rate 9999 --ping -n --randomize-hosts -v -e wlan0
 
-masscan -iL ip.txt --rate 9999 --ping -n --randomize-hosts -v -e wlan0
+masscan -iL ip.txt --rate 9999 --ping -n --randomize-hosts -v 
