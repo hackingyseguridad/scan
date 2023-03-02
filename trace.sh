58d03c825672757163638abcfb6fb7f65443e373
hackingyseguridad <antonio_taboada@hotmail.es>
2023-03-02 21:22:26 +0100

    Update trace.sh

--- trace.sh
+++ trace.sh
@@ -2,6 +2,6 @@
 # Script para testear ruta
 # Lee del fichero ip.txt la lista de IP
 # Uso: sh trace.sh
-# (C) hackingyseguridad.com 2017
+# (C) hackingyseguridad.com 2023
 echo
-for n in `cat ip.txt`; do echo $n; traceroute -A $n; done
+for n in `cat ip.txt`; do echo $n; traceroute -A -n -d -N 99 $n; done
