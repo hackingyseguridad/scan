36bf49582049f4fec1d31af3e7b414cd66b3e672
hackingyseguridad <antonio_taboada@hotmail.es>
2023-03-10 08:42:50 +0100

    Update rastreador.sh

--- rastreador.sh
+++ rastreador.sh
@@ -9,11 +9,11 @@ echo
 # Extrae IP de las rutas
 for n in `cat ip.txt`; do echo $n; traceroute -A -n -d -N 99 $n; done > resultado1.txt
 
-# filtra IP del resto de texto y caracteres
+# filtra IP del resto de texto y caracteres y genera resutado2.txt
 grep -Eo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" resultado1.txt > resultado2.txt
 
-# Elimina IP duplicadas
+# Elimina IP duplicadas y genera resultado3.txt
 awk '!visited[$0]++' "resultado2.txt" > resultado3.txt
 
-# Escanea las IP que hemos obtenido
+# Escanea las IP que hemos obtenido y genera resultado.txt 
 nmap -iL resultado3.txt -Pn -sVC --open -p 21,22,23,80,443 -oG resultado.txt
