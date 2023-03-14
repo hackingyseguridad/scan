6189fa6e5a1697352b3f62ca61b87e99c950fa96
hackingyseguridad <antonio_taboada@hotmail.es>
2023-03-14 15:13:45 +0100

    Update rastreador6.sh

--- rastreador6.sh
+++ rastreador6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script para rastrear rutas y escanerar los activos de los saltos que encuentra en la red
 # Script para IPv6
-# Lee del fichero ip.txt la lista de IP, entrega el resultado en el fichero resultado.txt
+# Lee del fichero ipv6.txt la lista de IP, entrega el resultado en el fichero resultado.txt
 # Uso: sh rastreador.sh
 # Version Beta
 # (C) hackingyseguridad.com 2023
@@ -9,7 +9,7 @@
 echo
 
 # Extrae IP de las rutas
-for n in `cat ip.txt`; do echo $n; traceroute -6 -A -n -d -N 99 $n; done > resultado1.txt
+for n in `cat ipv6.txt`; do echo $n; traceroute -6 -A -n -d -N 32 $n; done > resultado1.txt
 
 # filtra IP del resto de texto y caracteres y genera resutado2.txt
 egrep -Eo '(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))' resultado1.txt > resultado2.txt
