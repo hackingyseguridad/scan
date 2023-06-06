101ad066b97d0b31a5300042b6d223791a520453
hackingyseguridad <antonio_taboada@hotmail.es>
2023-06-06 18:33:04 +0200

    Update depura.sh

--- depura.sh
+++ depura.sh
@@ -4,10 +4,11 @@
 # Lee del fichero resultado.txt la lista de IP, entrega el resultado en el fichero resultado2.txt
 # Uso: sh depura.sh
 # (C) hackingyseguridad.com 2023
-
 echo
+echo ".."
 # Limpia de caracteres especiales el fichero de entrada ip.txt con direcciones IP IPv4
 grep -Eo "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" resultado.txt > resultado1.txt
-
+echo "..."
 # Elimina IP duplicadas y genera resultado3.txt
 awk '!visited[$0]++' "resultado1.txt" > resultado2.txt
+echo "... Ok"
