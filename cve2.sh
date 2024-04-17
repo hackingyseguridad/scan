808b8291746df7875dd32710d148ab003952779a
hackingyseguridad <antonio_taboada@hotmail.es>
2024-04-17 11:03:01 +0200

    Update cve2.sh

--- cve2.sh
+++ cve2.sh
@@ -1,3 +1,3 @@
-wget -4 https://github.com/scmanjarrez/CVEScannerV2/blob/master/cvescannerv2.nse
+# https://github.com/scmanjarrez/CVEScannerV2/
 sudo apt-get install lua5.1; sudo apt-get install lua-sql-sqlite3
 nmap -sV 192.168.1.1  --script=cvescannerv2.nse -F
