22630ad518012d73d2e20f8f02f17d8ad687c40c
hackingyseguridad <antonio_taboada@hotmail.es>
2024-10-11 10:59:19 +0200

    Update openproxy.sh

--- openproxy.sh
+++ openproxy.sh
@@ -6,12 +6,10 @@ echo
 for n in `cat proxy.txt`
 do 
         timeout 3;  curl -x http://$n:8081 -k --silent "http://ipecho.net/plain" \
--H 'Pragma: no-cache' \
--H 'Cache-Control: no-cache' \
 -H 'Upgrade-Insecure-Requests: 1' \
 -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36' \
 -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' \
 -H 'Accept-Encoding: gzip, deflate, br' \
--H 'Accept-Language: es-ES,es;q=0.9,en;q=0.8' \
+-H 'Accept-Language: es-ES,es;q=0.9,en;q=0.8' 
 done
 
