20f6e218616da70d9e6b815cfd679727abbfe4e0
hackingyseguridad <antonio_taboada@hotmail.es>
2025-01-02 18:19:25 +0100

    Update cve.sh

--- cve.sh
+++ cve.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 # https://github.com/scipag/vulscan
 # https://github.com/vulnersCom/nmap-vulners
+
+rm scipag_vulscan -R
+git clone https://github.com/scipag/vulscan scipag_vulscan
+ln -s `pwd`/scipag_vulscan /usr/share/nmap/scripts/vulscan
+
+rm nmap-vulners -R
+git clone https://github.com/vulnersCom/nmap-vulners
+ln -s `pwd`/nmap-vulners /usr/share/nmap/scripts/nmap-vulners
+
 nmap -iL ip.txt -Pn $1 $2 $3 $4 $5 -sS -sV --script=default,vuln -p- -T5
-nmap -iL ip.txt -Pn --open -sVUT -F -O --max-retries 1 -O  --defeat-rst-ratelimit --script=vulners.nse 
-nmap -iL ip.txt -Pn --open -sVUT -F -O --max-retries 1 -O  --defeat-rst-ratelimit --script=vulscan/vulscan.nse --script-args vulscandb=cve.csv
+nmap -iL ip.txt -Pn --open -sVUT -F -O  --defeat-rst-ratelimit --script=nmap-vulners/vulners.nse
+nmap -iL ip.txt -Pn --open -sVUT -F -O  --defeat-rst-ratelimit --script=vulscan/vulscan.nse --script-args vulscandb=cve.csv
+
