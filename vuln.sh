50db10573f8e26b89ceef000e85df550bb2c5f9b
hackingyseguridad <antonio_taboada@hotmail.es>
2025-01-03 19:13:40 +0100

    Update vuln.sh

--- vuln.sh
+++ vuln.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-nmap -Pn $1 $2 $3 $4 $5 -sV --script=default,vuln,vulners --script-args mincvss=7 -p- --open
+cat << "INFO"
+
+(:`--..___...-''``-._             |`._
+  ```--...--.      . `-..__      .`/ _\  
+            `\     '       ```--`.    />
+            : :   :               `:`-'
+             `.:.  `.._--...___     ``--...__      
+                ``--..,)       ```----....__,)   http://www.hackingyseguridad.com
+
+INFO
+if [ -z "$1" ]; then
+        echo
+        echo "Esaneo de puertos/servicios y vulnerabilidades conocidas."
+        echo "Requiere nmap"
+        echo "Uso.: sh vuln.sh <ip>"
+        echo
+        exit 0
+fi
+echo
+echo
+nmap -Pn $1 $2 $3 $4 $5 -sVC -O --script=banner,vuln,vulners --script-args mincvss=7 -p- --open 
