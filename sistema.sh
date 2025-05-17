3232be3fe13c442b72c05ed1bb8faecf1fe3de1f
hackingyseguridad <antonio_taboada@hotmail.es>
2025-05-17 21:59:53 +0200

    Update sistema.sh

--- sistema.sh
+++ sistema.sh
@@ -2,15 +2,18 @@
 #
 #  
 #
+echo 
 echo
-echo "Escanea sistemas operativos"
+echo "Escanea sistemas operativos...!"
+echo "http://www.hackingyseguridad.com"
 echo
-
-nmap -Pn $1 $2 $3 --randomize-hosts --max-retries 1 -n --open -sV -O --osscan-guess --script=banner -oG resultado.txt
+nmap -Pn $1 $2 $3 --randomize-hosts --max-retries 1 -n --open -sV -O --osscan-guess --script=banner -oN resultado.txt
 echo
 echo
 echo "= RESUMEN ==================================================================="
 echo
-cat resultado.txt 
+grep -E "Nmap scan report for|Running \(JUST GUESSING\):" resultado.txt | awk '/Nmap scan report for/{ip=$NF; next} /Running/{print ip, $0}' 
+
+
 
 
