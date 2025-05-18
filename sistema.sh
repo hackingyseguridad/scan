43ad75a0f220e6919fac372132356bc3f2466542
hackingyseguridad <antonio_taboada@hotmail.es>
2025-05-18 13:20:17 +0200

    Update sistema.sh

--- sistema.sh
+++ sistema.sh
@@ -1,25 +1,37 @@
-#!/bin/sh
-#
-#
-#
-#
+#!/bin/bash
+# Esaneo de sistema operativo y version.
+echo
+cat << "INFO"
+
+(:`--..___...-''``-._             |`._
+  ```--...--.      . `-..__      .`/ _\
+            `\     '       ```--`.    />
+            : :   :               `:`-'
+             `.:.  `.._--...___     ``--...__
+                ``--..,)       ```----....__,)   http://www.hackingyseguridad.com
+
+INFO
+if [ -z "$1" ]; then
+        echo
+        echo "Esaneo de puertos/servicios y vulnerabilidades conocidas."
+        echo "Requiere nmap"
+        echo "Uso.: sh vuln.sh <ip>"
+        echo
+        echo "\033[1;37mhttp://www.hackingyseguridad.com/\033[0m"
+        exit 0
+fi
 echo
 echo
-echo "\033[1;37mhttp://www.hackingyseguridad.com/\033[0m"
+echo "Escaneaabdo sistemas operativos...!"
 echo
 echo
-echo "Escanea sistemas operativos...!"
+nmap -Pn $1 $2 $3 -sV -p 445,135,139 --randomize-hosts -n -O --osscan-guess $0 -oG resultado.txt
 echo
 echo
-nmap -Pn $1 $2 $3  -sV -F --randomize-hosts -n -O --osscan-guess $0 -oN resultado.txt | sed -n '
-/Nmap scan report for/ {h}
-/OS details\|Aggressive OS guesses/ {x;G;s/\n/: /;p}
-'
+echo "=======================RESUKTADO======================"
 echo
-echo
-# echo "= RESUMEN ==================================================================="
-echo
-'
+cat resultado.txt
+
 
 
 
