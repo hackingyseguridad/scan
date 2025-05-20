4a18488f3150e14edaed6ea745e35ec482624d97
hackingyseguridad <antonio_taboada@hotmail.es>
2025-05-20 16:02:15 +0200

    Update so.sh

--- so.sh
+++ so.sh
@@ -1,6 +1,29 @@
+#!/bin/bash
+# Esaneo de sistema operativo y version.
+# (R) Antonio Taboada, 2025
+echo
+cat << "INFO"
 
+(:`--..___...-''``-._             |`._
+  ```--...--.      . `-..__      .`/ _\
+            `\     '       ```--`.    />
+            : :   :               `:`-'
+             `.:.  `.._--...___     ``--...__
+                ``--..,)       ```----....__,)   http://www.hackingyseguridad.com
 
-
+INFO
+if [ -z "$1" ]; then
+        echo
+        echo "Esaneo de sistema operativo y version."
+        echo "Requiere nmap"
+        echo "Uso.: sh so.sh <ip>"
+        echo
+        echo "\033[1;37mhttp://www.hackingyseguridad.com/\033[0m"
+        exit 0
+fi
+echo
+echo "Escaneaabdo sistemas operativos...!"
+echo
 
 nmap -Pn -F $1 $2 $3 --open -sV -O --osscan-guess $0 
 
