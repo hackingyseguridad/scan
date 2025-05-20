f6729ab59ca32ae898c53cf637d7602a2b20bb2b
hackingyseguridad <antonio_taboada@hotmail.es>
2025-05-20 16:43:13 +0200

    Update sistema2.sh

--- sistema2.sh
+++ sistema2.sh
@@ -28,6 +28,20 @@ nmap -Pn $1 $2 $3 -F --randomize-hosts -n -O --osscan-guess $0 -oN resultado.txt
 echo
 echo "======================= RESULTADO ========================="
 echo
+ grep -E -A10 "Nmap scan report for|Running \(JUST GUESSING\):|Aggressive OS guesses:" resultado.txt | awk '                                                                             
+  /Nmap scan report for/ { ip = $5 }
+  /Running \(JUST GUESSING\):/ { print ip, $0; next }
+' | sed 's/Nmap scan report for //; s/Running (JUST GUESSING): //;'
+
+
+
+echo
+echo
+
+
+
+
+
 grep -E -A10 "Nmap scan report for|Running \(JUST GUESSING\):|Aggressive OS guesses:" resultado.txt | awk '
   /Nmap scan report for/ { ip = $5 }
   /Running \(JUST GUESSING\):/ { print ip, $0; next }
