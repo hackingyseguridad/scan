a35d142ce7dd03efd19bbd44be92db80d52323c3
hackingyseguridad <antonio_taboada@hotmail.es>
2025-06-18 09:33:08 +0200

    Update scanipv6.sh

--- scanipv6.sh
+++ scanipv6.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Escaneo rapido IPv6
-# (c) hackingyseguridad.com 2025
+# http://www.hackingyseguridad.com  - 2025
 # Uso: ./scanipv6.sh
+# -min-rate 9999 , abre 10000 hilos
 # 
-#
-#
-nmap -6 -e eth0 -Pn 2a02:9000::/23 --open -n -p 22 --min-rate 5000  -oG resultado.txt
+# 
+nmap -6 -e eth0 -Pn 2a02:9000::/23 --open -n -p 22 --min-rate 9999 -oG resultado.txt
