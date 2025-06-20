269232d059197df6f726090a3a2050cd6b3eeb18
hackingyseguridad <antonio_taboada@hotmail.es>
2025-06-20 13:44:09 +0200

    Update scanipv6.sh

--- scanipv6.sh
+++ scanipv6.sh
@@ -3,6 +3,6 @@
 # http://www.hackingyseguridad.com  - 2025
 # Uso: ./scanipv6.sh
 # -min-rate 9999 , abre 10000 hilos
-# 
+#  solo alive nohup nmap -6  2a02:9000::/23 --open -n -sn --min-rate 9999 -oG resultado.txt
 # 
 nmap -6 -e eth0 -Pn 2a02:9000::/23 --open -n -p 22 --min-rate 9999 -oG resultado.txt
