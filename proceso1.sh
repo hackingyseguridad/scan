8a4ab032889b6f56ff521728d9b236dbf527a720
hackingyseguridad <antonio_taboada@hotmail.es>
2025-09-19 11:18:38 +0200

    Update proceso1.sh

--- proceso1.sh
+++ proceso1.sh
@@ -1,16 +1,49 @@
-# Si encuentra vulnerabilidades en el restuldado.txt, envia por email lo encontrado!
-#  2025
-
 #!/bin/sh
+# Para ejcutar como proceso: nohup ./proceso.sh &
+# Ejecuta escaneo de puertos y genera reporte informe.htm
+# ps -ALL |grep nmap
+# Uso.: sh proceso.sh IP &
+echo
+cat << "INFO"
 
-nmap -iL todas -F -PE -sTV --open -O -n --randomize-hosts --max-retries 2 -O --osscan-guess $0 --min-rate 1000 -oG resultado.txt -oX index.xml
+-=[ Proceso escaneo TCP a web ]=- @hackgyseguridad
+                          _____________
+                         |.-----------.|
+ hackingyseguridad.com   ||     _     ||
+    (c) 2025             ||   (\o/)   ||
+                         ||    /_\    ||
+ Escaneo de puertos,     ||___________||
+ con nmap que convierte,  `-)-------(-'
+ el resultado XML a web ,  [=== -- o ]--.
+ en formato html         __'---------'__ \
+                        [::::::::::: :::] )
+http://goo.gl/ID8XBX     `""'"""""'""""`/T\
+                                        \_/
+INFO
+echo
+echo
 
-# Archivo resultado.txt
-file="resultado.txt"
+mkdir /var/www/html/reports
+sudo apt-get install nmap
+sudo apt-get install xsltproc
 
-# Comprobar si el fichero contiene vulnerabilidades CVE
-if grep -q "CVE" "$file"; then
-    # Enviar correo electrónico si se encuentra la palabra CVE
-    echo "Vulnerabilidades CVE" | mail -s "Alerta: CVE encontrado" admin@localhost
-fi
+while :
+contador=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+do nmap $1 -PE -sV -iL todas.txt --open -O -n --defeat-rst-ratelimit --randomize-hosts --max-retries 2 -O --osscan-guess --defeat-rst-ratelimit --script=default,banner,vuln,vulners --script-args mincvss=8 -oX index.xml -oN $contador
+sudo xsltproc index.xml -o /var/www/html/reports/informe.htm
+echo
+echo "Generaldo fichero con resultado.: "$contador
+echo
+cp $contador resultado.txt
+rm $contador
 
+# file="resultado.txt"
+#if grep -q "CVE" "$file"; then
+# echo "Vulnerabilidades CVE" | mail -s "Alerta: CVE encontrado" admin@localhost
+# fi
+
+echo
+echo "Presiona [CTRL+C] para parar..."
+sleep 3
+echo
+done
