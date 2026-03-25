1eb774b437c76f0dff3177d7b4390902fd52329f
hackingyseguridad <antonio_taboada@hotmail.es>
2026-03-25 16:11:06 +0100

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,2 +1,2 @@
 # Escanea Node.JS
-nmap -Pn $1 $2 $3 $4 -p 3000,3001,5000,8080,8081,8000,1337,4200  -sV --script=http-headers,http-devframework -T4 
+nmap -Pn $1 $2 $3 $4 -p 3000,3001,5000,8080,8081,8000,1337,4200  -sV --script=http-headers,http-devframework -T4 --open
