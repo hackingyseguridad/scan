b23f4c25dfa9cf3cc3af475e64665b8fa725b8ca
hackingyseguridad <antonio_taboada@hotmail.es>
2026-05-02 17:34:33 +0200

    Update so.sh

--- so.sh
+++ so.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Esaneo de sistema operativo y version.
-# (R) Antonio Taboada, 2025
+# @antonio_taboada  2026
 echo
 cat << "INFO"
 
@@ -24,8 +24,31 @@ fi
 echo
 echo "Escaneaabdo sistemas operativos...!"
 echo
+nmap -Pn -F "$1" $2 $3 --open -sV -O --osscan-guess -oN resultado.txt -v0
 
-nmap -Pn -F $1 $2 $3 --open -sV -O --osscan-guess $0 
+echo "============================================================"
+echo "IP/HOST             | SISTEMA OPERATIVO / KERNEL"
+echo "============================================================"
+
+awk '
+/Nmap scan report for/ {
+    if (host != "" && info != "") printf "%-18s | %s\n", host, info
+    host = $5; gsub(/[()]/, "", host)
+    info = ""
+}
+/OS details/ && info == "" { info = substr($0, index($0,":")+2); sub(/^ /,"",info) }
+/Running:.*Linux/ && info == "" { info = substr($0, index($0,"R")); sub(/^ /,"",info) }
+/Linux [0-9]+\.[0-9]+/ && info == "" {
+    match($0, /Linux [0-9]+\.[0-9]+/); info = substr($0, RSTART, RLENGTH)
+}
+/Windows/ && info == "" {
+    match($0, /Windows[^,]+/); info = substr($0, RSTART, RLENGTH)
+}
+END { if (host != "" && info != "") printf "%-18s | %s\n", host, info }
+' resultado.txt
+
+echo "============================================================"
+echo
 
 
 
